3e541a134d09e2b9f2e018378ef542968090874e
Kyle123455 <muyotreydonk@gmail.com>
2025-02-05 13:45:16 -0800

    Create script.js

---
+++ script.js
@@ -0,0 +1,23 @@
+// Smooth scrolling for anchor links
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// Add animations on scroll
+const animateOnScroll = () => {
+  const elements = document.querySelectorAll('.animate__animated');
+  elements.forEach(element => {
+    const elementTop = element.getBoundingClientRect().top;
+    const windowHeight = window.innerHeight;
+    if (elementTop < windowHeight - 100) {
+      element.classList.add('animate__fadeIn');
+    }
+  });
+};
+
+window.addEventListener('scroll', animateOnScroll);
